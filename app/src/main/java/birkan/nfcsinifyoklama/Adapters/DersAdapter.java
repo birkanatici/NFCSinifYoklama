795333a37a5b903d53f9f6df7eac25db8edef719
Birkan Atıcı <birkanatici@yandex.com>
2017-12-24 18:07:12 +0300

    updated gradle to 3.0.1 and compile sdk to 26.

--- app/src/main/java/birkan/nfcsinifyoklama/Adapters/DersAdapter.java
+++ app/src/main/java/birkan/nfcsinifyoklama/Adapters/DersAdapter.java
@@ -1,5 +1,6 @@
 package birkan.nfcsinifyoklama.Adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
